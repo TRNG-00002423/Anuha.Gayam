40f983753eeeb6a74e2177043ca7db4737269171
StumbledUpon-CS <anuhagayam@gmail.com>
2026-06-09 19:13:06 -0700

    Completed Classwork Exercise Control Flow

---
+++ Week_1/3_WEDNSDAY/Exercises/classwork/control_flow_initial.py
@@ -0,0 +1,79 @@
+"""
+control-flow.md : 
+Involves :
+- Classwork, Wednesday June 3rd, Week 1 - Day 3
+"""
+
+Score = 85
+
+if (Score>=90):
+    grade = 'A'
+elif (Score>=80):
+    grade = 'B'
+elif (Score>=70):
+    grade = 'C'
+# elif (Score>=60):
+#     grade = 'D'
+else:
+    grade = 'F'
+
+
+print(f"Score : {Score} ---> Grade : {grade}")
+print("")
+print("")
+
+# Ternary Operator
+grade='PASS' if (Score>=60) else 'FAIL'
+
+
+SCORE = [95,85,75,65,55]
+
+for score in [95,85,75,65,55]:
+    if (score>=90):
+        grade = 'A'
+    elif (score>=80):
+        grade = 'B'
+    else:
+        grade = 'F'
+    print(f"Score : {score} ---> Grade : {grade}")
+
+
+
+print("")
+print("")
+print("")
+
+
+tests = ["login", "search", "checkout", "ignore"]
+
+for x in tests:
+    print(x.upper())
+
+while tests:
+    print(x.upper())
+
+
+numbers = [4, 5, 7, 2, -5, 9, -2, 5, -3, 1, 0, 8]
+
+# for number in numbers:
+#     match number:
+#         case number if (number<0):
+#             print("A")
+#             print(f"{number}")
+#             # continue
+#         case number if (number>0):
+#             print(f"{number}")
+#         case number if (number==0):
+#             break
+#         # case _:
+#         #     continue
+
+
+
+# for number in numbers:
+#     if (number>0):
+#         print(f"{number}")
+#     elif (number<0):
+#         print(f"Skipping : {number}")
+#     if (number==0):
+#         break
