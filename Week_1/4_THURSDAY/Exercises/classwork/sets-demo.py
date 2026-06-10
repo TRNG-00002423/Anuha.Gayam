0aa5800f1cba2fcedb39e1c1dc905b15bd6a6eec
StumbledUpon-CS <anuhagayam@gmail.com>
2026-06-10 00:48:41 -0700

    Completed Sets Demo

---
+++ Week_1/4_THURSDAY/Exercises/classwork/sets-demo.py
@@ -0,0 +1,35 @@
+# Sets : Mutable, Unordered, Unique
+
+
+print(" ")
+print(" ")
+
+ids = {1,2,3,4,5}
+
+
+empty_set = set()
+
+
+numbers = {1,2,2,3,4,5,3}
+from_list = set([10,20,20,30])  # Creating A Set From A List
+
+
+ids = {1,2,3}
+ids.add(4)
+ids.add(2)
+
+
+ids.remove(1)
+# ids.remove(100)             # KeyError
+
+
+ids.discard(2)
+ids.discard(100)              # No Error
+
+
+val = ids.pop()
+
+ids.clear()
+
+print(" ")
+print(" ")
